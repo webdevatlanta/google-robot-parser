26e662ff0b4509b0bd41e03408a66b8b8ba22026
Christopher Adams <chrismichaeladams@gmail.com>
2019-08-14 20:53:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: "" };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        console.log("A name was submitted: " + this.state.value);
+
+        let valParsed = this.state.value.split(" ").join("+");
+
+        valParsed = "https://www.google.com/search?q=" + valParsed;
+
+        console.log("val parsed", valParsed);
+
+        // const options = {
+        //     uri: valParsed,
+        //     transform: function(body) {
+        //         return cheerio.load(body);
+        //     }
+        // };
+
+        // rp(options)
+        //     .then($ => {
+        //         console.log($);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <>
+                <h2>Ask the Magic 8-Ball a question</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </>
+        );
+    }
 }
-
-export default App;
